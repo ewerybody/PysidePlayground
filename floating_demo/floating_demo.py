05fda4dc784865c1cf5c3a58e9dc337c815b1f2e
Eric Werner <ewerybody+github@gmail.com>
2020-07-06 20:03:11 +0200

    hello world!
    pushed the layer thing to a separate widget demo zoo

---
+++ floating_demo/floating_demo.py
@@ -0,0 +1,39 @@
+from PySide2 import QtCore, QtWidgets
+import floating_demo_ui
+
+
+class Demo(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Demo, self).__init__()
+
+        w = QtWidgets.QWidget(self)
+        self.setCentralWidget(w)
+        lyt = QtWidgets.QFormLayout(w)
+
+        self.lw = QtWidgets.QWidget(self)
+        self.ui = floating_demo_ui.Ui_Form()
+        self.ui.setupUi(self.lw)
+
+        self.ui.gridLayout.removeItem(self.ui.layout_1)
+        self.ui.gridLayout.removeItem(self.ui.layout_0)
+
+        # the order of layout adding is not important!
+        # what counts is the order of widget creation!
+        # the label needs to be created first! Then the button Voila!
+        self.ui.gridLayout.addLayout(self.ui.layout_0, 0, 0)
+        self.ui.gridLayout.addLayout(self.ui.layout_1, 0, 0)
+        self.ui.layout_1.setAlignment(
+            self.ui.toolButton, QtCore.Qt.AlignBottom|QtCore.Qt.AlignRight)
+
+        lyt.addRow(self.lw)
+
+
+def show():
+    app = QtWidgets.QApplication([])
+    win = Demo()
+    win.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    show()
---
+++ floating_demo/floating_demo_ui.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# Form implementation generated from reading ui file:
+#   'floating_demo.ui'
+# licensing of that file applies.
+#
+# Created: Mon Jul  6 19:51:16 2020
+#      by: pyside2-uic  running on PySide2 5.15.0
+#
+# pylint: disable=W0201,C0103,C0111
+# WARNING! All changes made in this file will be lost!
+
+from PySide2 import QtCore, QtWidgets
+
+
+class Ui_Form:
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(809, 441)
+        self.gridLayout = QtWidgets.QGridLayout(Form)
+        self.gridLayout.setSizeConstraint(QtWidgets.QLayout.SetMinimumSize)
+        self.gridLayout.setObjectName("gridLayout")
+        self.layout_0 = QtWidgets.QVBoxLayout()
+        self.layout_0.setContentsMargins(-1, 0, -1, -1)
+        self.layout_0.setObjectName("layout_0")
+        self.scrollArea = QtWidgets.QScrollArea(Form)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 635, 415))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.label = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        self.label.setWordWrap(True)
+        self.label.setObjectName("label")
+        self.verticalLayout_2.addWidget(self.label)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.layout_0.addWidget(self.scrollArea)
+        self.gridLayout.addLayout(self.layout_0, 0, 0, 1, 1)
+        self.layout_1 = QtWidgets.QVBoxLayout()
+        self.layout_1.setContentsMargins(-1, -1, 40, 40)
+        self.layout_1.setObjectName("layout_1")
+        self.toolButton = QtWidgets.QPushButton(Form)
+        self.toolButton.setMinimumSize(QtCore.QSize(100, 50))
+        self.toolButton.setMaximumSize(QtCore.QSize(50, 50))
+        self.toolButton.setObjectName("toolButton")
+        self.layout_1.addWidget(self.toolButton)
+        self.gridLayout.addLayout(self.layout_1, 0, 1, 1, 1)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QtWidgets.QApplication.translate("Form", "Form", None, -1))
+        self.label.setText(QtWidgets.QApplication.translate("Form", "TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, TextLabel, ", None, -1))
+        self.toolButton.setText(QtWidgets.QApplication.translate("Form", "Floater", None, -1))
