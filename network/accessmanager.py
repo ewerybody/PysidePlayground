5acd2a574b1f0ba09febfdccfa5eb81dd0462398
Eric Werner <ewerybody+github@gmail.com>
2020-09-27 12:21:32 +0200

    solved the error signal connection, added some ui
    ... for canceling and making the thing delayed on purpose

--- network/accessmanager.py
+++ network/accessmanager.py
@@ -4,7 +4,6 @@ from PySide2 import QtWidgets, QtNetwork, QtCore
 import ui_file
 
 DEFAULT_URL = 'https://www.qt.io'
-DEFAULT_TOTAL = 100000
 DEFAULT_TEXT = 'nothing looked up yet'
 
 
@@ -15,10 +14,15 @@ class QNetworkAccessManagerDemo(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.ui.url.setText(DEFAULT_URL)
         self.ui.button.clicked.connect(self.lookup)
+        self.ui.url.returnPressed.connect(self.lookup)
+        self.ui.cancel_button.clicked.connect(self._on_cancel)
         self._chunks = 0
         self._t0 = 0
         self.set_label(DEFAULT_TEXT)
         self.ready_ui()
+
+        self._error_connected = False
+        self._canceled = False
 
     def lookup(self):
         self.disable_ui()
@@ -29,8 +33,10 @@ class QNetworkAccessManagerDemo(QtWidgets.QMainWindow):
         if not url:
             return
 
+        print('\nLooking up "%s" ...' % url)
         self._t0 = time.time()
         self._chunks = 0
+        self._canceled = False
         manager = QtNetwork.QNetworkAccessManager(self)
         # manager.finished[QtNetwork.QNetworkReply].connect(self._on_finish)
         request = QtNetwork.QNetworkRequest(QtCore.QUrl(url))
@@ -40,35 +46,42 @@ class QNetworkAccessManagerDemo(QtWidgets.QMainWindow):
         reply.downloadProgress.connect(self._on_progress)
         reply.error[QtNetwork.QNetworkReply.NetworkError].connect(self._on_error)
         reply.sslErrors.connect(self._on_error)
-        # reply.errorOccured.connect(self._on_error)
         reply.readyRead.connect(self._on_ready_read)
 
-    def _on_ready_read(self, *stuff):
-        stuff
-        sender = self.sender()
-        sender
+        self._error_connected = False
+        self._try_error_connection(reply)
 
     def _on_progress(self, received, total):
+        if self._canceled:
+            reply = self.sender()
+            reply.abort()
+
         if received == 0 and total == 0:
             return
+
         if total == -1:
-            total = DEFAULT_TOTAL
-        self.ui.progress.setValue(int(received/total))
+            prog_value = 1
+        else:
+            prog_value = int(100 * received/total)
+        print('received/total/%%: %s %s %s%%' % (received, total, prog_value))
+
+        self.ui.progress.setValue(prog_value)
         self._chunks += 1
         self.set_label(f'{received} bytes received in {self._chunks} chunks')
-
-    def _on_error(self, error):
-        self.set_label(f'Error: {error.name.decode()}')
-        self.ready_ui()
+        self._visual_delay()
 
     def _on_finish(self):
         reply = self.sender()
-        # error = reply.errorString()
-        # if error:
-        #     self.set_label(f'Error: {error}')
-        # else:
-        self.append_label(f' in {time.time() - self._t0:.2f} seconds.')
+        self.append_label(f' <i> - in {time.time() - self._t0:.2f} seconds.</i>')
+        self._visual_delay()
         self.ready_ui()
+
+    def _on_ready_read(self):
+        # currently doing nothing ...
+        reply = self.sender()
+        thread = reply.thread()
+        reply.size()
+        self._try_error_connection(reply)
 
     def set_label(self, text):
         self.ui.size_label.setText(text)
@@ -78,15 +91,56 @@ class QNetworkAccessManagerDemo(QtWidgets.QMainWindow):
 
     def ready_ui(self, *stuff):
         self.ui.progress.hide()
+        self.ui.cancel_button.hide()
         for widget in (self.ui.button, self.ui.url):
             widget.setEnabled(True)
+            widget.blockSignals(False)
+        self.ui.url.setFocus()
 
     def disable_ui(self):
         self.set_label(DEFAULT_TEXT)
         for widget in (self.ui.button, self.ui.url):
             widget.setEnabled(False)
+            widget.blockSignals(True)
         self.ui.progress.setValue(0)
+        self.ui.cancel_button.show()
         self.ui.progress.show()
+
+    def _on_error(self, error):
+        reply = self.sender()
+        self.set_label(f'<b>Error</b>: {error.name.decode()}<br>'
+                       f'<b>errorString</b>: {reply.errorString()}')
+        self.ready_ui()
+
+    def _visual_delay(self):
+        if self.ui.delay_check.isChecked():
+            QtWidgets.QApplication.instance().processEvents()
+            time.sleep(self.ui.delay_time.value())
+
+    def _try_error_connection(self, reply):
+        """
+        Hmm. Seems we're unable to connect to a "errorOccured" while in flight.
+        As soon as we debug the signal is there and connectable ... weird.
+        The "error" one works tho!
+        """
+        if self._error_connected:
+            return
+
+        for signal_name in ('error', 'errorOccured'):
+            if not hasattr(reply, signal_name):
+                continue
+            signal = getattr(reply, signal_name)
+            try:
+                signal.connect(self._on_error)
+                self._error_connected = True
+                print(f'Connected to error signal: "{signal_name}"')
+            except AttributeError:
+                # print('Weird. No "errorOccured" Signal yet...')
+                self._error_connected = False
+
+    def _on_cancel(self):
+        self._canceled = True
+        self
 
 
 def main():
--- network/accessmanager_ui.py
+++ network/accessmanager_ui.py
@@ -3,16 +3,13 @@
 ################################################################################
 ## Form generated from reading UI file 'accessmanager.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.0
+## Created by: Qt User Interface Compiler version 5.15.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QMetaObject,
-    QObject, QPoint, QRect, QSize, QTime, QUrl, Qt)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
-    QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter,
-    QPixmap, QRadialGradient)
+from PySide2.QtCore import *
+from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
 
@@ -20,7 +17,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(794, 167)
+        MainWindow.resize(794, 265)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.formLayout = QFormLayout(self.centralwidget)
@@ -29,12 +26,6 @@ class Ui_MainWindow(object):
         self.url.setObjectName(u"url")
 
         self.formLayout.setWidget(0, QFormLayout.FieldRole, self.url)
-
-        self.progress = QProgressBar(self.centralwidget)
-        self.progress.setObjectName(u"progress")
-        self.progress.setValue(24)
-
-        self.formLayout.setWidget(2, QFormLayout.SpanningRole, self.progress)
 
         self.size_label = QLabel(self.centralwidget)
         self.size_label.setObjectName(u"size_label")
@@ -50,6 +41,40 @@ class Ui_MainWindow(object):
         self.label_2.setObjectName(u"label_2")
 
         self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label_2)
+
+        self.cancel_button = QPushButton(self.centralwidget)
+        self.cancel_button.setObjectName(u"cancel_button")
+
+        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.cancel_button)
+
+        self.progress = QProgressBar(self.centralwidget)
+        self.progress.setObjectName(u"progress")
+        self.progress.setValue(24)
+
+        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.progress)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.delay_check = QCheckBox(self.centralwidget)
+        self.delay_check.setObjectName(u"delay_check")
+
+        self.horizontalLayout.addWidget(self.delay_check)
+
+        self.delay_time = QDoubleSpinBox(self.centralwidget)
+        self.delay_time.setObjectName(u"delay_time")
+        self.delay_time.setDecimals(1)
+        self.delay_time.setMinimum(0.000000000000000)
+        self.delay_time.setMaximum(100.000000000000000)
+        self.delay_time.setValue(0.300000000000000)
+
+        self.horizontalLayout.addWidget(self.delay_time)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+
+        self.formLayout.setLayout(3, QFormLayout.FieldRole, self.horizontalLayout)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
@@ -70,5 +95,8 @@ class Ui_MainWindow(object):
         self.size_label.setText("")
         self.button.setText(QCoreApplication.translate("MainWindow", u"Get Size", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"URL to look up:", None))
+        self.cancel_button.setText(QCoreApplication.translate("MainWindow", u"Cancel", None))
+        self.delay_check.setText(QCoreApplication.translate("MainWindow", u"deliberately delay", None))
+        self.delay_time.setSuffix(QCoreApplication.translate("MainWindow", u"s", None))
     # retranslateUi
 
